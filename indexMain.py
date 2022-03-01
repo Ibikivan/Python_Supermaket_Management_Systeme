90c5e21fe458b188d6694634fd4cf8b87c56d4ea
ibikiv <ibikivan1@gmail.com>
2022-03-01 17:38:34 +0100

    Model de départ pour l'écriture coopérative du code

--- indexMain.py
+++ indexMain.py
@@ -2,4 +2,28 @@ import copy
 # J'ai importé copy à l'avance
 # Début du programme. C'est à partir d'ici qu'on écrira le code
 
-print("Hello World !")
+# On commence par préparer les différents models de données à manipuler
+# On crée les models de produits et clients
+productsModel = {
+    "id" : 0,
+    "name" : "",
+    "quantity" : 0,
+    "price" : 0
+}
+
+clientsModel = {
+    "id" : 0,
+    "name" : "",
+    "totalExpenses" : 0.0,
+    "fidelityPoints" : 0
+}
+
+# puis on crée la liste catalogue et une liste vierge de clients
+productCatalog = []
+clientsList = []
+
+# Vous pouvez écrire vos fonctions à partir d'ici
+# Merci d'écrire de façon propre et de commenter vos codes
+# Une ligne d'espace entre les blocs de code et deux entre les grands blocs de code
+# Une ligne d'espace avant les commentaires et pas de ligne d'espace en dessous, entre le commentaire et le bloc de code associé
+
