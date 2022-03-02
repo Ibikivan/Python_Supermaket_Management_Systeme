6ed318b3bfd88473744cffbbe33a686e5816cc32
ibikiv <ibikivan1@gmail.com>
2022-03-02 20:29:58 +0100

    fonctionalité d'affichage d'une listes des produits achetables avec les points d'un client

--- indexMain.py
+++ indexMain.py
@@ -29,36 +29,42 @@ clientsList = []
 # Une ligne d'espace entre les blocs de code et deux entre les grands blocs de code
 # Une ligne d'espace avant les commentaires et aucun espace en dessous, entre le commentaire et le bloc de code associé
 
-# Créons la fonctionnalité de listage des produits dont la quantité en stock est inférieure à 10
-# Pour les besoins des tests je crée un catalogue de quelques produits (une fonction que j'appelle plusieurs fois)
-def clientCreator(id, name, expenses):
-    currentClient = copy.deepcopy(clientsModel)
-    currentClient["id"] = id
-    currentClient["name"] = name
-    currentClient["totalExpenses"] = expenses
-    clientsList.append(currentClient)
-    return currentClient
-
-
 # Maintenant je crée ma fonction qui va m'indiquer les produit achetables avec mes points
+# Pour les besoins des tests je crée un catalogue de quelques produits (une fonction que j'appelle plusieurs fois)
+def productCreator(ref, name, stock, price):
+    currentProduct = copy.deepcopy(productsModel)
+    currentProduct["ref"] = ref
+    currentProduct["name"] = name
+    currentProduct["quantity"] = stock
+    currentProduct["price"] = price
+    productCatalog.append(currentProduct)
+    return currentProduct
+
+
 # pour commencer je crée une fonction qui va calculer l'équivalence des points en argent
 def howMuchItWorth(points):
-    # Ici une fonction va être appelée et son résultat retourné dans une variable. Pas de paramètres au final
+    # Ici une fonction va être appelée pour récupérer les dépenses du client et calculer ses points et son résultat
+    # retourné dans une variable Pas de paramètres nécessaires dans la version définitive de cette fonction
     pointsValue = points * 100
     return pointsValue
 
 
 # ensuite je crée une fonction qui récupère ce nombre de point et affiche les produits qui y équivalent
 def couldIBuyList():
-    for
+    montant = howMuchItWorth(10)
+    cheap = 1
+    print("voici les produits que vous pouvez acheter actuellement avec vos points :")
+    for product in productCatalog:
+        if product["price"] <= montant:
+            print("{}. {} -> prix: {}; quantité restante: {}".format(cheap, product["name"], product["price"],
+                                                                     product["quantity"]))
+            cheap += 1
 
 
-clientCreator(1, "ibikiv", 30000)
-clientCreator(2, "dylan", 25000)
-clientCreator(3, "jacques", 110000)
-clientCreator(4, "marina", 18000)
-clientCreator(5, "sanson", 534000)
+productCreator("ba01", "banana", 50, 200)
+productCreator("ma01", "mango", 50, 100)
+productCreator("rul01", "ruler", 50, 50)
+productCreator("phone01", "smart Phone", 50, 30000)
+productCreator("tle01", "table", 50, 15000)
 
-
-
-print(howMuchItWorth(50))
+couldIBuyList()
